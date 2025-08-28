a2c5878080ab7f97ce13386d5e5b1f4458eeca80
Mekha <mohanananmekha04@gmail.com>
2025-08-28 21:17:33 +0530

    bookings are added

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store.jsx'
-
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <StrictMode>
 
     <App />
-  </Provider>
+  </StrictMode>,
 )
